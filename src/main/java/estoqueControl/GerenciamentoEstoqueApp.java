07419e7eab0e2050ceff75999984002ddc855ae9
p12000216 <p12000216@brasilprev.com.br>
2023-03-18 02:55:48 -0300

    18 de março 02:55

---
+++ src/main/java/estoqueControl/GerenciamentoEstoqueApp.java
@@ -0,0 +1,14 @@
+package estoqueControl;
+import javafx.application.Application;
+import javafx.stage.Stage;
+public class GerenciamentoEstoqueApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Crie a janela principal aqui
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
